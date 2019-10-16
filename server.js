a66a4613e9454b23c6067c711e5f03e07886737c
AlessandroPilleri <39701974+AlessandroPilleri@users.noreply.github.com>
2019-10-16 17:55:11 +0200

    Add functionality to server

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+const youtube = require('youtube-dl');
 
 app.use(express.static('public'));
 app.listen(8080);
 
 app.get('/background', function (req, res) {
-  
+
 })
 
-app.get('/download', function (req, res) {
+app.get('/video', function (req, res) {
+  // var url = req.body; // FIXME: video id from get params
+  var videoId = req.query.id;
 
+  var video = youtube('https://www.youtube.com/watch?v=' + videoId,
+    ['--format=18'],
+    { cwd: __dirname });
+
+  video.on('info', function (info) {
+    console.log('Download started');
+    console.log('filename: ' + info._filename);
+    console.log('size: ' + info.size);
+  });
+
+  video.on('end', function (info) {
+    console.log('Download ended');
+  })
+
+  video.pipe(fs.createWriteStream('video.mp4'));
+  res.send("OK");
 })
